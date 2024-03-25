d087fc76e0e2bf776432df9b7a18b06b468c0616
Raxzero <emircan.acikgoz@hotmail.com>
2024-03-25 11:10:06 +0300

    my first attempt at 4th algorithm

---
+++ 4. Median of Two Sorted Arrays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Algorithms
+{
+    internal class _4
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] combinedArray = nums1.Concat(nums2).ToArray();
+            Array.Sort(combinedArray);
+            int lengthOfArray = combinedArray.Length;
+            double medianValue;
+            if (lengthOfArray % 2 == 0)
+            {
+                medianValue = combinedArray[lengthOfArray/2];
+            }
+            else
+            {
+                int lowerMedian = (int)lengthOfArray/2 - 0,5;
+                int upperMedian = (int)lengthOfArray/2 + 0,5;
+                medianValue = combinedArray[lowerMedian] + combinedArray[upperMedian];
+            }
+            return medianValue;
+        }
+    }
+}
