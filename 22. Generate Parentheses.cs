e0f5160e6ba8a79bf2b366ca1c51f385ae245651
Raxzero <emircan.acikgoz@hotmail.com>
2024-06-26 10:10:43 +0300

    started 22th algorithm

---
+++ 22. Generate Parentheses.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Algorithms
+{
+    internal class _22
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            int totalNumberOfParentheses = MathFactorial(n);
+            IList<string> result = new List<string>();
+            StringBuilder newSetOfParentheses = new();
+            int numberOfOpenParenthses = 0;
+            
+            while (totalNumberOfParentheses > 0)
+            {
+                if (numberOfOpenParenthses < 0)
+                {
+                    newSetOfParentheses.Append("(");
+                    totalNumberOfParentheses--;
+                }
+                else
+                {
+                    newSetOfParentheses.Append(')');
+                    totalNumberOfParentheses--;
+                }
+                result.Add(newSetOfParentheses.ToString());
+            }
+        }
+
+
+        // Unrelated algorithm to the problem I spontenaously wrote
+        public int MathFactorial(int num)
+        {
+            if (num > 1)
+            {
+                int result = (num * num - 1);
+                num = num - 2;
+
+                while (num > 1)
+                {
+                    result = result * (num - 1);
+                    num--;
+                }
+                return result;
+                }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/LeetCode Algorithms.AssemblyInfo.cs
+++ obj/Debug/net8.0/LeetCode Algorithms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5191a26a996ed27c5ad1a85dbebf2cd8bb8826c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e1f627a55abe9fa7b52ac9d21cf8182178d37c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
