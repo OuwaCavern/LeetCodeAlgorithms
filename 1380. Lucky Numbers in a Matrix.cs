e4570fd720906ec49ac4dd5efc5d3d71f4a4dece
OuwaCavern <emircan.acikgoz@hotmail.com>
2024-07-19 22:25:29 +0300

    first attempt at 1380.algorithm

---
+++ 1380. Lucky Numbers in a Matrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Algorithms
+{
+    // https://leetcode.com/problems/lucky-numbers-in-a-matrix/description/?envType=daily-question&envId=2024-07-19
+    internal class _1380
+    {
+        public IList<int> LuckyNumbers(int[][] matrix)
+        {
+            int n = matrix.Length;
+            int m = matrix[0].Length;
+
+            IList<int> result = new List<int>();
+
+            List<int> minimumOfEachRow = new List<int>();
+            List<int> maximumOfEachColumn = new List<int>();
+
+            minimumOfEachRow.Add(matrix[0][0]);
+            maximumOfEachColumn.Add(matrix[0][0]);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i][j] >= maximumOfEachColumn.Max())
+                    {
+                        maximumOfEachColumn.RemoveAt(maximumOfEachColumn.Count - 1);
+                        maximumOfEachColumn.Add(matrix[i][j]);
+                    }
+                    if (matrix[i][j] <= minimumOfEachRow.Min())
+                    {
+                        minimumOfEachRow.RemoveAt(minimumOfEachRow.Count - 1);
+                        maximumOfEachColumn.Add(matrix[i][j]);
+                    }
+                }
+            }
+
+            foreach (int i in minimumOfEachRow)
+            {
+                foreach (int j in maximumOfEachColumn)
+                {
+                    if (i == j)
+                    {
+                        result.Add(i);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
--- obj/Debug/net8.0/LeetCode Algorithms.AssemblyInfo.cs
+++ obj/Debug/net8.0/LeetCode Algorithms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87356e41a7e1bbfe84ac92e41ae7f612f0f24110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485e07fd1d0dee252f219660636de198360ce12c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
