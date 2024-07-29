3933228a4255f5ef5b37dcdfcaea3395e43c5081
Raxzero <emircan.acikgoz@hotmail.com>
2024-07-29 18:05:29 +0300

    my first attempt on 1395th algorithm

---
+++ 1395. Count Number of Teams.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Algorithms
+{
+    internal class _1395
+    {
+        //https://leetcode.com/problems/count-number-of-teams/?envType=daily-question&envId=2024-07-29
+        public int NumTeams(int[] rating)
+        {
+            List<Queue<int>> allTeams = new List<Queue<int>>();
+            Queue<int> teams = new Queue<int>();
+            bool isAscending = false;
+
+            foreach (int i in rating)
+            {
+                teams.Enqueue(i);
+                if (teams.Count == 1)
+                {
+                    if (teams.Max() > i)
+                    {
+                        isAscending = false;
+                    }
+                    else if (teams.Max() < 1)
+                    {
+                        isAscending = true;
+                    }
+
+                }
+                else if (teams.Count == 2)
+                {
+                    if (isAscending && teams.Max() < i)
+                    {
+                        allTeams.Add(teams);
+                    }
+                    else if (!isAscending && teams.Max() > i)
+                    {
+                        allTeams.Add(teams);
+                    }
+                    else
+                    {
+                        teams.Dequeue();
+                    }
+                }
+            }
+            return allTeams.Count;
+        }
+    }
+}
--- obj/Debug/net8.0/LeetCode Algorithms.AssemblyInfo.cs
+++ obj/Debug/net8.0/LeetCode Algorithms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485e07fd1d0dee252f219660636de198360ce12c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dac62a546e9eb4350d977b29f256eb22f43a422")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
