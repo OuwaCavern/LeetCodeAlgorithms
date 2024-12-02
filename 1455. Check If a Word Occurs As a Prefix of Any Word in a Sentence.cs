007acef7168b939307ea9eb6c5d015a4f7df1dd4
Emircan AÇIKGÖZ <emircan.acikgoz@yemekpos.com>
2024-12-02 11:01:25 +0300

    solved the 1455th algorithm

---
+++ 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Algorithms
+{
+    internal class _1455
+    {
+        //https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+        internal int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            string[] wordsOfTheSentence = sentence.Split(" ");
+
+            int whichWord = 0;
+
+            foreach (string word in wordsOfTheSentence)
+            {
+                whichWord++;
+
+                if (word.IndexOf(searchWord) == 0)
+                {
+                    return whichWord;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Linq;
 using LeetCode_Algorithms;
 using static LeetCode_Algorithms._2;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Sandbox
 {
@@ -10,40 +11,44 @@ namespace Sandbox
     {
         public static void Main(string[] args)
         {
-            _1 twosumalgorithm = new _1();
-            twosumalgorithm.TwoSum([3, 2, 4], 6);
-            ListNode l1 = new ListNode(9);
-            ListNode l2 = new ListNode(1);
-            l2.next = new ListNode(9);
-            l2.next.next = new ListNode(9);
-            l2.next.next.next = new ListNode(9);
-            l2.next.next.next.next = new ListNode(9);
-            l2.next.next.next.next.next = new ListNode(9);
-            l2.next.next.next.next.next.next = new ListNode(9);
-            l2.next.next.next.next.next.next.next = new ListNode(9);
-            l2.next.next.next.next.next.next.next.next = new ListNode(9);
-            l2.next.next.next.next.next.next.next.next.next = new ListNode(9);
-            _2 addtwonumbersalgo = new _2();
-            addtwonumbersalgo.AddTwoNumbers(l1, l2);
-            _3 longestSubstring = new _3();
-            longestSubstring.LengthOfLongestSubstring("pwwkew");
-            _4 medianAlgo = new _4();
-            medianAlgo.FindMedianSortedArrays([1, 2], [3, 4]);
-            _713 subarrayProductAlgo = new _713();
-            subarrayProductAlgo.NumSubarrayProductLessThanK([10, 5, 2, 6], 100);
-            Solution solution = new Solution();
-            _5 palindromeAlgo = new();
-            palindromeAlgo.LongestPalindrome("ccc");
-            _8 atoiAlgo = new();
-            atoiAlgo.MyAtoi("1234567890123456789012345678901234567890");
-            //_7 reverseintAlgo = new();
-            //reverseintAlgo.Reverse(-2654);
-            _14 commonPrefixAlgo = new();
-            commonPrefixAlgo.LongestCommonPrefix(["flower", "flow", "flight"]);
-            _20 parenthesesAlgo = new();
-            parenthesesAlgo.IsValid("{[]}");
-            _28 firstStringAlgo = new();
-            firstStringAlgo.StrStr("mississippi", "issip");
+            //_1 twosumalgorithm = new _1();
+            //twosumalgorithm.TwoSum([3, 2, 4], 6);
+            //ListNode l1 = new ListNode(9);
+            //ListNode l2 = new ListNode(1);
+            //l2.next = new ListNode(9);
+            //l2.next.next = new ListNode(9);
+            //l2.next.next.next = new ListNode(9);
+            //l2.next.next.next.next = new ListNode(9);
+            //l2.next.next.next.next.next = new ListNode(9);
+            //l2.next.next.next.next.next.next = new ListNode(9);
+            //l2.next.next.next.next.next.next.next = new ListNode(9);
+            //l2.next.next.next.next.next.next.next.next = new ListNode(9);
+            //l2.next.next.next.next.next.next.next.next.next = new ListNode(9);
+            //_2 addtwonumbersalgo = new _2();
+            //addtwonumbersalgo.AddTwoNumbers(l1, l2);
+            //_3 longestSubstring = new _3();
+            //longestSubstring.LengthOfLongestSubstring("pwwkew");
+            //_4 medianAlgo = new _4();
+            //medianAlgo.FindMedianSortedArrays([1, 2], [3, 4]);
+            //_713 subarrayProductAlgo = new _713();
+            //subarrayProductAlgo.NumSubarrayProductLessThanK([10, 5, 2, 6], 100);
+            //Solution solution = new Solution();
+            //_5 palindromeAlgo = new();
+            //palindromeAlgo.LongestPalindrome("ccc");
+            //_8 atoiAlgo = new();
+            //atoiAlgo.MyAtoi("1234567890123456789012345678901234567890");
+            ////_7 reverseintAlgo = new();
+            ////reverseintAlgo.Reverse(-2654);
+            //_14 commonPrefixAlgo = new();
+            //commonPrefixAlgo.LongestCommonPrefix(["flower", "flow", "flight"]);
+            //_20 parenthesesAlgo = new();
+            //parenthesesAlgo.IsValid("{[]}");
+            //_28 firstStringAlgo = new();
+            //firstStringAlgo.StrStr("mississippi", "issip");
+
+            _1455 prefixAlgo = new _1455();
+            Console.WriteLine(prefixAlgo.IsPrefixOfWord("i love eating burger", "burg"));
+            Console.ReadLine();
         }
     }
 }
