b987f5508c4c889244cd3590fb5e90e1e9584700
Emircan AÇIKGÖZ <emircan.acikgoz@yemekpos.com>
2025-05-27 13:28:22 +0300

    solved the 2894th problem

---
+++ 2894. Divisible and Non-divisible Sums Difference.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Algorithms
+{
+    public class _2894
+    {
+        // https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/description/?envType=daily-question&envId=2025-05-27
+        public int DifferenceOfSums(int n, int m)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            while (n > 0)
+            {
+                if (n % m == 0)
+                {
+                    num2 += n;
+                    n--;
+                }
+                else
+                {
+                    num1 += n;
+                    n--;
+                }
+            }
+            return num1 - num2;
+        }
+    }
+}
