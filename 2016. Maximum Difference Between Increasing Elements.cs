9398389a7e6d5bb6c17322bc8ee6075db75c4527
Emircan AÇIKGÖZ <emircan.acikgoz@yemekpos.com>
2025-06-17 10:33:19 +0300

    solved the 2016th problem

---
+++ 2016. Maximum Difference Between Increasing Elements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Algorithms
+{
+    public class _2016
+    {
+        // https://leetcode.com/problems/maximum-difference-between-increasing-elements/description
+        public int MaximumDifference(int[] nums)
+        {
+            int length = nums.Length;
+            int currentMaxDifference = -1;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if (i > j) 
+                        continue;
+                    else if (nums[j] > nums[i] && (nums[j] - nums[i] > currentMaxDifference))
+                    {
+                        currentMaxDifference = nums[j] - nums[i];
+                    }
+                }
+            }
+
+            return currentMaxDifference;
+        }
+    }
+}
